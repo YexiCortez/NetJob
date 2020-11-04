4b62c6e60aac38b9e76642113c64c0e0913513f9
billy270 <billly223@gmail.com>
2020-11-04 10:08:37 -0600

    Actualización pag Registro

--- lib/P_registro.dart
+++ lib/P_registro.dart
@@ -6,26 +6,52 @@ class RegistroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold
     (
+      appBar: AppBar(  title: Text('hola') ),
+
+      
       body : Column 
       (
         //holalasldlas
         children: <Widget>
         [
+          // aqui debe ir el logo 
           Container
           (
-            height: 500,
-            width: 430,
-            color: Colors.red,
+            height: 200,
+            width: 200,
           ),
-          Positioned(
-            child: Container(
-              height: 54,
-              decoration: BoxDecoration
+
+            Positioned
+            (
+              bottom:0,
+              left: 0,
+              right: 0,
+              child: Container
               (
-                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                height: 54,
+                decoration: BoxDecoration
+                (
+                  color: Colors.white,
+                  borderRadius:  BorderRadius.circular(20), 
+                  border:Border.all
+                  (
+                    color: Colors.lightBlueAccent,
+                    width: 4,
+                  ),
+                  boxShadow:
+                  [
+                    BoxShadow
+                    (
+                      offset: Offset(0,10),
+                      blurRadius: 50,
+                      color: Colors.black.withOpacity(0.23),
+                    )
+                  ]
+                  
+                ),
               ),
             ),
-          ),
         ]
       )
     );
