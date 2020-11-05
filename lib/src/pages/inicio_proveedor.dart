fd6a715cf4e66fd100f038beb4c7cd9cd9f5e627
Yexi_Cortez <yexili92@gmail.com>
2020-11-05 14:14:31 -0500

    inicio proveedor

--- lib/src/pages/inicio_proveedor.dart
+++ lib/src/pages/inicio_proveedor.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 
+class InicioProveedor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _buildAppLogo()
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildAppLogo(){
+  return Container(
+    width: 175.0,
+    height: 175.0,
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        fit: BoxFit.fill,
+        image: AssetImage('assets/img/Netjob-logoicono.png')),
+      shape: BoxShape.circle,
+    ),
+  );
+}
+}
