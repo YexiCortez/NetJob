13e6291c77185bd7ce6dc6f1d9fce369f18148f9
billy270 <billly223@gmail.com>
2020-11-16 21:16:12 -0500

    ...

---
+++ lib/src/opciones.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class optionSccreen extends StatelessWidget {
+  const optionSccreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold
+    (
+      appBar: AppBar(title:Text('Opciones')),
+
+      body: Column(
+        children: <Widget>[
+
+          Container(
+            
+          )
+
+        ],
+      ),
+
+      
+    );
+  }
+}
